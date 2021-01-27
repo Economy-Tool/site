52a3f7d1d427bddb25a10ed7b86bf17dad9df4ee
AlbericoD <albericodiasbarretofilho@gmail.com>
2021-01-27 16:40:02 -0300

    added new workflow.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,15 +8,15 @@
  */
 
 module.exports = {
-  title: 'My Site',
+  title: 'Economy Tool',
   tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://economy-tool.github.io',
+  baseUrl: '/site/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Economy-Tool', // Usually your GitHub org/user name.
+  projectName: 'site', // Usually your repo name.
   themeConfig: {
     navbar: {
       title: 'My Facebook Project',
